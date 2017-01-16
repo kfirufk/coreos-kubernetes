63e0b736c9e3861c110becf1b318b20202c17623
Promaethius <bryant.jonathan.42@gmail.com>
2017-01-16 12:09:55 -0800

    Revert "rkt automatically mounts resolv.conf conflict"
    This reverts commit 493fe1cf54029dfac0ffbca8737adeea361ad2a1.

--- multi-node/generic/controller-install.sh
+++ multi-node/generic/controller-install.sh
@@ -914,6 +914,9 @@ spec:
             - mountPath: /var/run/calico
               name: var-run-calico
               readOnly: false
+            - mountPath: /etc/resolv.conf
+              name: dns
+              readOnly: true
         # This container installs the Calico CNI binaries
         # and CNI network config file on each node.
         - name: install-cni
@@ -958,6 +961,9 @@ spec:
         - name: cni-net-dir
           hostPath:
             path: /etc/kubernetes/cni/net.d
+        - name: dns
+          hostPath:
+            path: /etc/resolv.conf
 
 ---
 
