9b01ab68abac3232ac52936d3f8bbca4b4fda888
Kfir Ozer <kfirufk@gmail.com>
2017-03-04 15:06:32 +0000

    bug fixes

--- multi-node/generic/controller-install/install-kube.sh
+++ multi-node/generic/controller-install/install-kube.sh
@@ -5,6 +5,7 @@ set -o errexit
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ENV_FILE="env.sh"
+KUBECTL_BIN=/opt/bin/kubectl
 
 source ${DIR}/${ENV_FILE}
 
@@ -16,7 +17,7 @@ echo "installing kubectl..."
 mkdir -p /opt/bin
 
 curl -o /opt/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${K8S_VER::-9}/bin/linux/amd64/kubectl
-chmod +x /opt/bin/kubectl
+chmod +x ${KUBECTL_BIN}
 
 echo "done installing kubectl"
 
@@ -71,16 +72,16 @@ function start_addons {
 
     echo
     echo "K8S: DNS addon"
-    kubectl apply -f /srv/kubernetes/manifests/kube-dns-de.yaml -f /srv/kubernetes/manifests/kube-dns-svc.yaml -f /srv/kubernetes/manifests/kube-dns-autoscaler-de.yaml --namespace kube-system
+    ${KUBECTL_BIN} apply -f /srv/kubernetes/manifests/kube-dns-de.yaml -f /srv/kubernetes/manifests/kube-dns-svc.yaml -f /srv/kubernetes/manifests/kube-dns-autoscaler-de.yaml --namespace kube-system
 
     echo "K8S: Heapster/InfluxDB/Graphana addon"
-    kubectl apply -f /srv/kubernetes/manifests/heapster-influx-graphana-de.yaml,/srv/kubernetes/manifests/heapster-influx-graphana-svc.yaml
+    ${KUBECTL_BIN} apply -f /srv/kubernetes/manifests/heapster-influx-graphana-de.yaml,/srv/kubernetes/manifests/heapster-influx-graphana-svc.yaml
     echo "K8S: Kube-Lego addon"
-    kubectl apply -f /srv/kubernetes/manifests/kube-lego.yaml
+    ${KUBECTL_BIN} apply -f /srv/kubernetes/manifests/kube-lego.yaml
 	echo "K8S: NGinx Ingress addon"
-    kubectl apply -f /srv/kubernetes/manifests/ingress-nginx.yaml -f /srv/kubernetes/manifests/default-backend.yaml
+    ${KUBECTL_BIN} apply -f /srv/kubernetes/manifests/ingress-nginx.yaml -f /srv/kubernetes/manifests/default-backend.yaml
     echo "K8S: Dashboard addon"
-    kubectl apply -f /srv/kubernetes/manifests/kube-dashboard-de.yaml -f /srv/kubernetes/manifests/kube-dashboard-svc.yaml --namespace kube-system
+    ${KUBECTL_BIN} apply -f /srv/kubernetes/manifests/kube-dashboard-de.yaml -f /srv/kubernetes/manifests/kube-dashboard-svc.yaml --namespace kube-system
    echo "finished starting addons"
 }
 
@@ -119,14 +120,14 @@ function install_ceph {
 	cd /home/core/generator
 	./generate_secrets.sh all `./generate_secrets.sh fsid`
 	
-	kubectl create namespace ceph
-	kubectl create secret generic ceph-conf-combined --from-file=ceph.conf --from-file=ceph.client.admin.keyring --from-file=ceph.mon.keyring --namespace=ceph
-	kubectl create secret generic ceph-bootstrap-rgw-keyring --from-file=ceph.keyring=ceph.rgw.keyring --namespace=ceph
-	kubectl create secret generic ceph-bootstrap-mds-keyring --from-file=ceph.keyring=ceph.mds.keyring --namespace=ceph
-	kubectl create secret generic ceph-bootstrap-osd-keyring --from-file=ceph.keyring=ceph.osd.keyring --namespace=ceph
-	kubectl create secret generic ceph-client-key --from-file=ceph-client-key --namespace=ceph
+	${KUBECTL_BIN} create namespace ceph
+	${KUBECTL_BIN} create secret generic ceph-conf-combined --from-file=ceph.conf --from-file=ceph.client.admin.keyring --from-file=ceph.mon.keyring --namespace=ceph
+	${KUBECTL_BIN} create secret generic ceph-bootstrap-rgw-keyring --from-file=ceph.keyring=ceph.rgw.keyring --namespace=ceph
+	${KUBECTL_BIN} create secret generic ceph-bootstrap-mds-keyring --from-file=ceph.keyring=ceph.mds.keyring --namespace=ceph
+	${KUBECTL_BIN} create secret generic ceph-bootstrap-osd-keyring --from-file=ceph.keyring=ceph.osd.keyring --namespace=ceph
+	${KUBECTL_BIN} create secret generic ceph-client-key --from-file=ceph-client-key --namespace=ceph
 	
-	kubectl create \
+	${KUBECTL_BIN} create \
 	-f /srv/kubernetes/manifests/ceph-osd.yaml \
 	-f /srv/kubernetes/manifests/ceph-mon.yaml \
 	-f /srv/kubernetes/manifests/ceph-mds.yaml \
@@ -143,7 +144,7 @@ function start_calico {
         sleep 5
     done
     echo "Deploying Calico"
-    kubectl apply -f /srv/kubernetes/manifests/calico-config.yaml,/srv/kubernetes/manifests/calico.yaml
+    ${KUBECTL_BIN} apply -f /srv/kubernetes/manifests/calico-config.yaml,/srv/kubernetes/manifests/calico.yaml
     echo "finished starting calico"
 }
 
@@ -172,4 +173,4 @@ if [ $USE_CALICO = "true" ]; then
 fi
 start_addons
 install_ceph
-cho "DONE"
+echo "DONE"
