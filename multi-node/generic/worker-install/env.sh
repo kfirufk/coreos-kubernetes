f41f0b68271d3b739c816d676a79bbc1aa2fc9cd
Kfir Ozer <kfirufk@gmail.com>
2017-03-22 20:03:47 +0000

    updated K8S_VER on worker

--- multi-node/generic/worker-install/env.sh
+++ multi-node/generic/worker-install/env.sh
@@ -2,7 +2,7 @@
 export ETCD_ENDPOINTS=http://127.0.0.1:4001
 export CONTROLLER_ENDPOINT=https://coreos-2.tux-in.com
 export ADVERTISE_IP=192.168.1.3
-export K8S_VER=v1.5.4_coreos.0
+export K8S_VER=v1.6.0-beta.3_coreos.0
 export HYPERKUBE_IMAGE_REPO=quay.io/coreos/hyperkube
 export POD_NETWORK=10.2.0.0/16
 export DNS_SERVICE_IP=10.3.0.10
